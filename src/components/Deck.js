4dc6131b194001ea7d0b414bb432e685d562bfd4
byio <ben@benyang.io>
2018-03-26 07:10:53 +0800

    create Deck component

---
+++ src/components/Deck.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Animated, View } from 'react-native';
+
+class Deck extends Component {
+  // render method
+  render () {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Deck;

aa8b9bbc84c6e50681cc3708f94f0fae95f00ffc
byio <ben@benyang.io>
2018-03-25 22:21:15 +0800

    render Ball component in App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Ball from './src/components/Ball';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world!</Text>
+        <Ball />
       </View>
     );
   }

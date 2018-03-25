d37e0cb78915b10b4ad0ef6e3129f8dfef95cbcc
byio <ben@benyang.io>
2018-03-26 07:33:41 +0800

    receive data and renderCard props in renderCards method in Deck component

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { Animated, View } from 'react-native';
 
 class Deck extends Component {
+  // helper methods
+    /*
+      the renderCards method will receive data and
+      renderCard props
+    */
+  renderCards () {
+    return this.props.data.map (item => {
+      return this.props.renderCard(item);
+    });
+  }
+
   // render method
   render () {
     return (

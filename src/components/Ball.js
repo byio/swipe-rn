575eca425045d123daf9da1a7f9e6038deb8631a
byio <ben@benyang.io>
2018-03-25 22:15:55 +0800

    create Ball component

---
+++ src/components/Ball.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Ball extends Component {
+  // render method
+  render () {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Ball;
